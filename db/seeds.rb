3b6a7d78345a3994cb718c584289439a39bb0a10
Biondo Anthony <anthony.biondo@kedgebs.com>
2024-03-11 17:04:15 +0100

    Seed api SuperHero

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "json"
+require "open-uri"
+
+user_seed = User.new(email: "anthins.errt@gmail.com", password: "azerty")
+user_seed.save!
+
+url = "https://akabab.github.io/superhero-api/api/all.json"
+heroes = URI.open(url).read
+hero = JSON.parse(heroes)
+
+hero.each do |h|
+  Superhero.create!(name: h["name"], team: h["biography"]["publisher"], image_url: h["images"]["md"], user_id: user_seed.id)
+end
