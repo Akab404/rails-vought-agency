936ec137548453271e50df224978d96921fd8f7c
florian wagrez <florian.wagrez@gmail.com>
2024-03-14 13:50:43 +0100

    review and account

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,28 @@
 class ReviewsController < ApplicationController
+  before_action :set_superhero
+
   def new
     @review = Review.new
+  end
+
+  def create
+    @review = Review.new(reviews_params)
+    @review.superhero = @superhero
+    @review.user = current_user
+    if @review.save!
+      redirect_to superhero_path(@superhero)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_superhero
     @superhero = Superhero.find(params[:superhero_id])
+  end
+
+  def reviews_params
+    params.require(:review).permit(:comment, :rating)
   end
 end
