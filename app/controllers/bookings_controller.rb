3ef85567373567d4b92c898ad11ba60fdd567a2c
florian wagrez <florian.wagrez@gmail.com>
2024-03-15 11:59:59 +0100

    Validation des bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,19 +14,37 @@ class BookingsController < ApplicationController
   def create
     @superhero = Superhero.find(params[:superhero_id])
     @booking = Booking.new(booking_params)
+    @booking.accepted = nil
     @booking.superhero = @superhero
     @booking.user = current_user
-    if @booking.save!
-      redirect_to superhero_path(@superhero)
-      flash[:alert] = "Vous avez bien reservé #{@superhero.name}"
-    else
-      render 'superheros/show', status: :unprocessable_entity
-    end
+    booking_save
   end
 
+  def accept
+    @booking = Booking.find(params[:id])
+    @booking.update!(accepted: true)
+    redirect_to users_account_path, status: :see_other
+    flash[:alert] = "You accepted the reservation of #{@booking.user.email} made on #{@booking.superhero.name}"
+  end
+
+  def decline
+    @booking = Booking.find(params[:id])
+    @booking.update!(accepted: false)
+    redirect_to users_account_path, status: :see_other
+    flash[:alert] = "You declined the reservation of #{@booking.user.email} made on #{@booking.superhero.name}"
+  end
 end
 
 private
+
+def booking_save
+  if @booking.save!
+    redirect_to superhero_path(@superhero)
+    flash[:alert] = "Vous avez bien reservé #{@superhero.name}"
+  else
+    render 'superheros/show', status: :unprocessable_entity
+  end
+end
 
 def booking_params
   params.require(:booking).permit(:checkin, :checkout)
---
+++ app/javascript/controllers/totalprice_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="totalprice"
+export default class extends Controller {
+  static targets = ["price", "checkin", "checkout", "totalPrice"]
+  connect() {
+    console.log("coucou ça fonctionne");
+  }
+
+  calcul(){
+    // 132 => price
+    // 13.03.2024
+    // 1.03.2024
+    const priceInt = parseInt(this.priceTarget.innerHTML, 10)
+    const checkinMonth = parseInt(this.checkinTarget.value.split("-")[1], 10)
+    const checkoutMonth = parseInt(this.checkoutTarget.value.split("-")[1], 10)
+    const checkinDay = parseInt(this.checkinTarget.value.split("-")[0], 10)
+    const checkoutDay =  parseInt(this.checkoutTarget.value.split("-")[0], 10)
+
+
+    const countedMonth = checkoutMonth - checkinMonth
+
+    if (countedMonth == 0) {
+      const total = priceInt*(checkoutDay - checkinDay)
+      this.totalPriceTarget.innerHTML = `${total} €`
+    } else {
+      const total = priceInt*((30*countedMonth)-(checkoutDay- checkinDay))
+      this.totalPriceTarget.innerHTML =  `${total} €`
+    }
+  }
+}
